e51bcce6576f1e24868e0b305f57b8d9c516f3b4
Keyur Patel <ekeyur@gmail.com>
2019-03-07 12:50:37 -0500

    completed comment box component with tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default() => {
-  return <div>Comment Box</div>
+class CommentBox extends Component {
+  state = {
+    comment: ''
+  };
+  handleChange = event => {
+    this.setState({comment: event.target.value})
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({comment: ''})
+    //call the action creator And save the comment
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea id="text" onChange={this.handleChange} value={this.state.comment}/>
+        <div>
+          <button className="button">Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
 }
+
+export default CommentBox;
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CommentBox from 'components/CommentBox';
+import {mount} from 'enzyme';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = mount(<CommentBox/>);
+})
+afterEach(() => {
+  wrapper.unmount();
+})
+
+it('has a text area and a button', () => {
+  expect(wrapper.find('#text').length).toEqual(1);
+  expect(wrapper.find('.button').length).toEqual(1);
+})
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapper.find('#text').simulate('change', {
+      target: { value: 'new comment' }
+    });
+  wrapper.update();
+  })
+
+  it('has a text area that users can type in', () => {
+    expect(wrapper.find('#text').prop('value')).toEqual('new comment');
+  })
+  
+  it('changes the value of text area to empty when form is submitted', () => {
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('#text').prop('value')).toEqual('');
+  })
+})
+
